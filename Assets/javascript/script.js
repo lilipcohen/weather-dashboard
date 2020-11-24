e6578c2214a53aec6ed24a220eac2b68bc0ef0fc
Lili Cohen <lili.cohen.tv@gmail.com>
2020-11-24 10:07:04 -0800

    working city search insert and adding dynamic data elements

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,8 +1,10 @@
 var submitBtn = $("button");
-var inputCity = $("input");
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&id=524901&appid=9ed816d0a4838a096ac31df49691a77a";
 
-function searchCity() { 
+
+function displayCityInfo() { 
+    var inputCity = $("#inlineFormInputCity").val().trim();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&id=524901&appid=9ed816d0a4838a096ac31df49691a77a";
+
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -12,6 +14,16 @@ function searchCity() {
     
             // Storing an array of results in the results variable
             console.log(response);
+
+            var cityName = $("<h2>").text(response.name + response.coord.dt);
+            $(".city-choice").prepend(cityName);
+            
+            var cityTemp = $("<p>").text(response.main.temp);
+            cityName.append(cityTemp);
+            var cityHum = $("<p>").text(response.main.humidity);
+            cityTemp.append(cityHum);
+            var cityWind = $("<p>").text(response.wind.speed);
+            cityHum.append(cityWind);
         });
 }
 
@@ -20,5 +32,5 @@ submitBtn.on("click", function (event) {
     event.preventDefault();
     console.log('event listener');
  
-    searchCity(inputcity);
+    displayCityInfo();
 });

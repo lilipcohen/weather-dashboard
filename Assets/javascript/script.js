2fb950e016034a130990d25c31b5fbeb8099dc82
Lili Cohen <lili.cohen.tv@gmail.com>
2020-11-28 14:33:09 -0800

    adding empty array

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,18 +2,21 @@ var submitBtn = $(".btn-primary");
 
 var API_SECRET = '&id=524901&appid=9ed816d0a4838a096ac31df49691a77a'
 
-var searchHistory = localStorage.getItem('searches') || []
+var searchHistory = localStorage.getItem('searches');
+var cityArr = [];
 
 function loadSearchButtons() {
     // TODO - Run a for-loop to add buttons to search area
      $("city-buttons").empty();
     var inputCity = $("#inlineFormInputCity").val().trim();
-
-    // TODO - Each button should have an event listener that calls the API functions
-        
-    var cityBtn = $("<button>").text(inputCity);
-    $(".city-buttons").append(cityBtn);
+    cityArr.push(cityBtn);
     
+    // TODO - Each button should have an event listener that calls the API functions
+    for (var i = 0; i < cityArr.length; i++) {
+        $("city-buttons").empty();
+        var cityBtn = $("<button>").text(inputCity);
+        $(".city-buttons").append(cityBtn);
+    }
     cityBtn.on("click", function (event) { 
         event.preventDefault();
         displayCityInfo();
@@ -30,34 +33,26 @@ function displayCityInfo() {
         url: queryURL,
         method: "GET"
     })
-        // After the data comes back from the API
         .then(function (response) {
     
             // Storing an array of results in the results variable            
 
             var cityName = $("<h2>").text(response.name);
-            $(".city-choice").append(cityName);
-            //date looks off
             var cityDate = $("<p>").text(new Date(response.dt * 1000).toDateString());
-            cityName.append(cityDate);
-
             var iconcode = response.weather[0].icon
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-
             var cityIcon = $("<img>").attr('src', iconurl);
-            cityDate.append(cityIcon);
-
             var temp = Math.floor((response.main.temp - 273) * (9/5) + 32)
             var cityTemp = $("<p>").text("Temperature: " + temp + '°F');
-            $(".city-choice").append(cityTemp);
-
             var cityHum = $("<p>").text("Humidity: " + response.main.humidity + '%');
-            $(".city-choice").append(cityHum);
-
             var cityWind = $("<p>").text("Wind Speed: " + (response.wind.speed * 2.237) + ' mph');
-            $(".city-choice").append(cityWind);
-            
             var uvUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}${API_SECRET}`
+            $(".city-choice").append(cityName);
+            cityName.append(cityDate);
+            cityDate.append(cityIcon);
+            $(".city-choice").append(cityTemp);
+            $(".city-choice").append(cityHum);
+            $(".city-choice").append(cityWind);
 
             $.ajax({
                 url: uvUrl,
@@ -141,17 +136,10 @@ submitBtn.on("click", function (event) {
     event.preventDefault();
     
     // TODO - Save the city to local storage
-    var inputCity = $("#inlineFormInputCity").val().trim();
     
-    localStorage.setItem('searches', inputCity) || []
+    localStorage.setItem('searches', searchHistory);
     displayCityInfo();
     displayFiveDay();
     loadSearchButtons();
 
 });
-
-//how to create search history and store information? local storage?
-
-// 1. When the page loads, get search history from local storage
-// 2. Load the buttons
-// 3. When you search for a city, save that search to local storage
